ffa4101d1199df05a3e89ed8be85eaa8ebb7625b
xinzheng <2689402871@QQ.COM>
2025-09-01 16:21:13 +0800

    去除springBoot

--- src/main/java/com/example/demo_peng/service/Impl/StringServerImpl.java
+++ src/main/java/com/example/demo_peng/service/Impl/StringServerImpl.java
@@ -2,17 +2,11 @@ package com.example.demo_peng.service.Impl;
 
 import com.example.demo_peng.handler.StringHandler;
 import com.example.demo_peng.service.StringServer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-
-@Service
 public class StringServerImpl implements StringServer {
 
-    @Autowired
     private StringHandler removeConsecutiveImpl;
 
-    @Autowired
     private StringHandler replaceConsecutiveImpl;
 
     public void removeConsecutive(String str){
